2b12d6c54d052c04eb1ec71810d8111286a3fd65
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-26 08:54:46 +0000

    F

--- src/database/profile.ts
+++ src/database/profile.ts
@@ -2,8 +2,8 @@ import mongoose, { Schema } from 'mongoose';
 import { IProfile } from '../utils/interface/database/profile';
 
 const profile: Schema = new Schema({
-	id: { type: String, unique: true, required: true},
-})
+    id: { type: String, unique: true, required: true },
+});
 
 const Profile = mongoose.model<IProfile>('profile', profile);
 export { Profile };
