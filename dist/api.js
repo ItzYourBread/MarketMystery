88bcad5d58dce2a0c5f7bb2b470de36ca9e7cc2b
Bread <72562619+NotRealArif@users.noreply.github.com>
2023-01-21 16:09:34 +0000

    Api updated!

--- dist/api.js
+++ dist/api.js
@@ -5,7 +5,14 @@ var express_1 = tslib_1.__importDefault(require("express"));
 var chalk_1 = tslib_1.__importDefault(require("chalk"));
 var stock_1 = require("./database/stock");
 require("dotenv/config");
+var express_session_1 = tslib_1.__importDefault(require("express-session"));
+var node_fetch_1 = tslib_1.__importDefault(require("node-fetch"));
 var app = (0, express_1.default)();
+app.use((0, express_session_1.default)({
+    secret: 'wishd827sjusbdlcjsbsisbs829jsjsbduwbz',
+    resave: false,
+    saveUninitialized: true,
+}));
 app.get('/api/stock/:ticker', function (req, res) { return tslib_1.__awaiter(void 0, void 0, void 0, function () {
     var ticker, stock;
     return tslib_1.__generator(this, function (_a) {
@@ -24,14 +31,45 @@ app.get('/api/stock/:ticker', function (req, res) { return tslib_1.__awaiter(voi
         }
     });
 }); });
-app.get('/api/login', function (req, res) {
-    var redirectUri = 'https://vue.subsidised.repl.co/';
+app.get('/login', function (req, res) {
+    var redirectUri = 'http://103.60.13.253:20306/callback/';
     var clientId = '943855772415193118';
     var scope = 'identify';
     var responseType = 'code';
     var url = "https://discord.com/oauth2/authorize?client_id=".concat(clientId, "&scope=").concat(scope, "&response_type=").concat(responseType, "&redirect_uri=").concat(redirectUri);
     res.redirect(url);
 });
+app.get('/callback', function (req, res) { return tslib_1.__awaiter(void 0, void 0, void 0, function () {
+    var code, redirectUri, clientId, clientSecret, url, response, json;
+    return tslib_1.__generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                code = req.query.code;
+                redirectUri = 'http://103.60.13.253:20306/callback/';
+                clientId = '943855772415193118';
+                clientSecret = '7gTEzqCK7zyNQprLcJhowsIhLYaE8jaF';
+                url = "https://discord.com/api/oauth2/token?grant_type=authorization_code&code=".concat(code, "&redirect_uri=").concat(redirectUri, "&client_id=").concat(clientId, "&client_secret=").concat(clientSecret);
+                return [4, (0, node_fetch_1.default)(url, { method: 'POST' })];
+            case 1:
+                response = _a.sent();
+                if (!response.ok) {
+                    res.status(response.status).send("Error: ".concat(response.statusText));
+                    return [2];
+                }
+                return [4, response.json()];
+            case 2:
+                json = _a.sent();
+                if (!json.access_token) {
+                    res.status(400).send('Error: Missing access_token');
+                    return [2];
+                }
+                req.session['access_token'] = json.access_token;
+                req.session.save();
+                res.redirect("https://vue.subsidised.repl.co/");
+                return [2];
+        }
+    });
+}); });
 app.listen(process.env.PORT, function () {
     console.log(chalk_1.default.greenBright("Server is running on port ".concat(process.env.PORT)));
 });
--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,18 @@ import express, { Request, Response } from 'express';
 import chalk from 'chalk';
 import { Stock } from './database/stock';
 import 'dotenv/config';
+import session from 'express-session';
+import fetch from 'node-fetch';
 
 const app = express();
+
+app.use(
+    session({
+        secret: 'wishd827sjusbdlcjsbsisbs829jsjsbduwbz',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 app.get('/api/stock/:ticker', async (req: Request, res: Response) => {
     const ticker = req.params.ticker;
@@ -15,14 +25,37 @@ app.get('/api/stock/:ticker', async (req: Request, res: Response) => {
     res.send(stock.history);
 });
 
-app.get('/api/login', (req: Request, res: Response) => {
-  const redirectUri = 'https://vue.subsidised.repl.co/';
-  const clientId = '943855772415193118';
-  const scope = 'identify';
-  const responseType = 'code';
-  const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&scope=${scope}&response_type=${responseType}&redirect_uri=${redirectUri}`;
-  res.redirect(url);
+app.get('/login', (req: Request, res: Response) => {
+    const redirectUri = 'http://103.60.13.253:20306/callback/';
+    const clientId = '943855772415193118';
+    const scope = 'identify';
+    const responseType = 'code';
+    const url = `https://discord.com/oauth2/authorize?client_id=${clientId}&scope=${scope}&response_type=${responseType}&redirect_uri=${redirectUri}`;
+    res.redirect(url);
 });
+
+app.get('/callback', async (req: Request, res: Response) => {
+    const code = req.query.code;
+    const redirectUri = 'http://103.60.13.253:20306/callback/';
+    const clientId = '943855772415193118';
+    const clientSecret = '7gTEzqCK7zyNQprLcJhowsIhLYaE8jaF';
+    const url = `https://discord.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}&client_id=${clientId}&client_secret=${clientSecret}`;
+
+    const response = await fetch(url, { method: 'POST' });
+    if (!response.ok) {
+        res.status(response.status).send(`Error: ${response.statusText}`);
+        return;
+    }
+    const json = await response.json();
+    if (!json.access_token) {
+        res.status(400).send('Error: Missing access_token');
+        return;
+    }
+    req.session['access_token'] = json.access_token;
+    req.session.save();
+    res.redirect("https://vue.subsidised.repl.co/");
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log(
